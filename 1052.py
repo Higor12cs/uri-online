f7fdb6d4c9962670de2f0e32b4ad2bda7c40af45
Higor Carneiro <higor12cs@gmail.com>
2020-06-15 15:53:00 -0300

    Exercicios resolvidos

---
+++ 1052.py
@@ -0,0 +1,39 @@
+#ok
+mes=int(input())
+
+if(mes==1):
+    print("January")
+elif(mes==2):
+    print("February")
+elif(mes==3):
+    print("March")
+elif(mes==4):
+    print("April")
+elif(mes==5):
+    print("May")
+elif(mes==6):
+    print("June")
+elif(mes==7):
+    print("July")
+elif(mes==8):
+    print("August")
+elif(mes==9):
+    print("September")
+elif(mes==10):
+    print("October")
+elif(mes==11):
+    print("November")
+else:
+    print("December")
+#January – Janeiro
+#February – Fevereiro
+#March – Março
+#April – Abril
+#May – Maio
+#June – Junho
+#July – Julho
+#August – Agosto
+#September – Setembro
+#October – Outubro
+#November – Novembro
+#December – Dezembro
