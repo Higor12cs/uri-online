ec55a33f80d820aded653c262904099a38fcd57e
Higor Carneiro <higor12cs@gmail.com>
2021-06-18 09:37:38 -0300

    1048.c

---
+++ 1048.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void){
+	float sal;
+
+	scanf("%f", &sal);
+
+	if(sal<=400)
+		printf("Novo salario: %.2f\nReajuste ganho: %.2f\nEm percentual: 15 %%\n", sal*1.15, (sal*1.15)-sal);
+	else if(sal<=800)
+		printf("Novo salario: %.2f\nReajuste ganho: %.2f\nEm percentual: 12 %%\n", sal*1.12, (sal*1.12)-sal);
+	else if(sal<=1200)
+		printf("Novo salario: %.2f\nReajuste ganho: %.2f\nEm percentual: 10 %%\n", sal*1.10, (sal*1.10)-sal);
+	else if(sal<=2000)
+		printf("Novo salario: %.2f\nReajuste ganho: %.2f\nEm percentual: 7 %%\n", sal*1.07, (sal*1.07)-sal);
+	else
+		printf("Novo salario: %.2f\nReajuste ganho: %.2f\nEm percentual: 4 %%\n", sal*1.04, (sal*1.04)-sal);
+}
